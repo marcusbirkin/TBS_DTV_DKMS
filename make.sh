93646f0393fc1be05b9d0a45f2332a31ad4eb0d1
marcusbirkin <marcusbirkin@users.noreply.github.com>
2018-04-13 16:06:34 +0100

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
+wget http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
+tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
+
+git submodule update --recursive --remote
+
 cd media_build 
 make dir DIR=../media KERNELRELEASE=$kernelver 
-make distclean 
+make allyesconfig 
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null) KERNELRELEASE=$kernelver
