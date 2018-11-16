9c6b20b3b04c0fb90bbcc5d5619d22cb523a5cde
marcusbirkin <marcusbirkin@users.noreply.github.com>
2018-11-16 18:32:19 +0000

    Update make.sh
    Allow non-dkms execution (generally for debuging)

--- make.sh
+++ make.sh
@@ -5,6 +5,7 @@ tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
 git submodule update --recursive --remote
 
 cd media_build 
+if [ -z "$kernelver" ]; then kernelver=`uname -r` ; fi
 make dir DIR=../media KERNELRELEASE=$kernelver 
 make allyesconfig 
 make -j$(grep -c ^processor /proc/cpuinfo 2>/dev/null) KERNELRELEASE=$kernelver
