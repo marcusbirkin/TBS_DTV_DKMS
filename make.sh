f5951894c3e4fd6848bc597e64d66f4bd4486046
marcusbirkin <marcusbirkin@users.noreply.github.com>
2020-03-22 16:27:03 +0000

    Update make.sh
    Don't re-retrieve firmware unless newer than local.

--- make.sh
+++ make.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-wget http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
+wget -N http://www.tbsdtv.com/download/document/linux/tbs-tuner-firmwares_v1.0.tar.bz2
 tar jxvf tbs-tuner-firmwares_v1.0.tar.bz2 -C /lib/firmware/
 
 git submodule update --recursive --remote
