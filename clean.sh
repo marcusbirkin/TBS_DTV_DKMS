1656afcab9a4e79e7b25f1e79877cd48a9766d90
marcusbirkin <marcusbirkin@users.noreply.github.com>
2020-03-22 17:23:22 +0000

    Update clean.sh
    More aggressive clean

--- clean.sh
+++ clean.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
+git remote update
+git pull
+git reset --hard
 git submodule foreach git remote update
 git submodule foreach git pull
 git submodule foreach git reset --hard
+git submodule foreach git clean -fx -fd
